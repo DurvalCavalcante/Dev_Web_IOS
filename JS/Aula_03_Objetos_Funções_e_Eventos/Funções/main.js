acebdd90298c5159e43bd82a8023e3ed45573256
DurvalCavalcante <durvalcavalcante00@gmail.com>
2022-05-02 22:00:48 -0300

    Aula 03 de JS

---
+++ JS/Aula_03_Objetos_Funções_e_Eventos/Funções/main.js
@@ -0,0 +1,54 @@
+//Estrutura de uma fução
+//Sempre quando for chamar uma função, usar sempre o "()".
+/*
+funtion NomedaFuncao (valor1,valor2){
+    return valor1 * valor2
+
+}
+*/
+
+//Funções
+function AddNums(num1 = 2, num2 = 5) {
+    return num1 + num2;
+}
+
+console.log(AddNums());
+
+//Se eu quiser alterar o valor das variável
+// let x = AddNums(10,11);
+// console.log(x);
+
+console.clear();
+
+//Criando Arrow Function
+const hello = (primeiro = 2, segundo = 2) => {
+    return primeiro * segundo;
+};
+
+console.log(hello());
+
+const somaAddNums = (num1 = 4, num2 = 5) => {
+    return num1 + num2;
+};
+
+console.log(somaAddNums());
+
+//Criando Objetos
+const aluno = {
+    primeiroNome: 'Durval',
+    segundoNome: 'Cavalcante',
+    idade: '23',
+};
+
+console.clear();
+
+//Criando Evento onload
+const boasVindas = () => {
+    alert('Bem-Vindo a nossa Pagina');
+    console.log('Bem-Vindo a nossa Pagina');
+};
+
+//Criando Evento onclick
+function eventoClick() {
+    console.log('Você clicou aqui!');
+};
