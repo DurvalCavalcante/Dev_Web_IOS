094db8a943718014075df1ecb2915eda97fe5328
DurvalCavalcante <durvalcavalcante00@gmail.com>
2022-05-05 16:23:37 -0300

    Aula 04 JS

--- JS/Aula_04_Arrays/Exercício_Aula_04/novosMetodos.js
+++ JS/Aula_04_Arrays/Exercício_Aula_04/novosMetodos.js
@@ -1,5 +1,21 @@
-console.log('Dois Novos Métodos')
+console.log('Dois Novos Métodos');
 
+console.log('\nLista de cores:');
 let cor = ['Preto', 'Azul', 'Rosa', 'Amarelo', 'Roxo'];
 console.log(`Cores: ${cor}`);
 
+// splice é usado para remover itens de uma posição de índice.
+
+let pos = 1, n = 2;
+
+console.log('\nRemovendo itens 1 e 2 da lista de cores.');
+console.log('Lista Atualizada:');
+let r = cor.splice(pos, n);
+console.log(`Cores: ${cor}`);
+
+// forEach mostra as posição dos índices.
+
+console.log('\nPosição dos índices da lista de cores:');
+let copiar = cor.forEach(function (item, indice, array) {
+    console.log(item, indice);
+});
