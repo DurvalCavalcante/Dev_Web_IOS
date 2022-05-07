2b8ec927e7ff58447e0a4dea908306025e5f9084
DurvalCavalcante <durvalcavalcante00@gmail.com>
2022-05-07 17:10:49 -0300

    Exercício de Lógica

--- JS/Lógica_De_JS/Exercício_de_Lógica_Parte_2/js_exercicio_03.js
+++ JS/Lógica_De_JS/Exercício_de_Lógica_Parte_2/js_exercicio_03.js
@@ -1,5 +1,12 @@
 console.log('Exercício JS 03');
 
+const carro = 25000; // Valor do carro
 
+const pd = 28 / 100 * carro; // Porcentagem do distribuidor
 
+const pi = 48 / 100 * carro; // Porcentagem dos impostos
 
+//Resultado
+let valor = pd + pi + carro;
+
+console.log(`O valor do carro de fábrica é: ${valor}`);
