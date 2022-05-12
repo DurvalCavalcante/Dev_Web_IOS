71299fc5827d2ee62c58962d2886095ac01b8112
DurvalCavalcante <durvalcavalcante00@gmail.com>
2022-05-11 21:52:18 -0300

    Aula 07 de JS

--- JS/Aula_06_Arrays_de_Objetos/Exercício_Aula_06/arraysObjetos.js
+++ JS/Aula_06_Arrays_de_Objetos/Exercício_Aula_06/arraysObjetos.js
@@ -17,9 +17,9 @@ const alunos = [
     {
         nome: 'Daniel',
         sobrenome: 'Do Nascimento',
-        sala: 3,
+        sala: 1,
         nota: [6, 8, 6, 6],
-        media: (6 + 8 + 6 + 6) / 4,        
+        media: (6 + 8 + 6 + 6) / 4,
         endereco: {
             rua: 'Rua Lero Lero',
             numero: 359,
@@ -30,7 +30,7 @@ const alunos = [
     {
         nome: 'Priscila',
         sobrenome: 'De Lima',
-        sala: 3,
+        sala: 2,
         nota: [8, 8, 6, 6],
         media: (8 + 8 + 6 + 6) / 4,
         endereco: {
@@ -41,10 +41,10 @@ const alunos = [
     },
 ];
 
-console.log(`\nO Aluno: ${alunos[0].nome} ${alunos[0].sobrenome} com as Notas: ${alunos[0].nota} Mora na ${alunos[0].endereco.rua} teve a Média: ${alunos[0].media}`);
-console.log(`\nO Aluno: ${alunos[1].nome} ${alunos[1].sobrenome} com as Notas: ${alunos[1].nota} Mora na ${alunos[1].endereco.rua} teve a Média: ${alunos[1].media}`);
-console.log(`\nA Aluna: ${alunos[2].nome} ${alunos[2].sobrenome} com as Notas: ${alunos[2].nota} Mora na ${alunos[2].endereco.rua} teve a Média: ${alunos[2].media}`);
+console.log(`\nO Aluno: ${alunos[0].nome} ${alunos[0].sobrenome} com as Notas: ${alunos[0].nota} Mora na ${alunos[0].endereco.rua} N° ${alunos[0].endereco.numero} Cidade: ${alunos[0].endereco.cidade} teve a Média: ${alunos[0].media}`);
+console.log(`\nO Aluno: ${alunos[1].nome} ${alunos[1].sobrenome} com as Notas: ${alunos[1].nota} Mora na ${alunos[1].endereco.rua} N° ${alunos[1].endereco.numero} Cidade: ${alunos[1].endereco.cidade} teve a Média: ${alunos[1].media}`);
+console.log(`\nA Aluna: ${alunos[2].nome} ${alunos[2].sobrenome} com as Notas: ${alunos[2].nota} Mora na ${alunos[2].endereco.rua} N° ${alunos[2].endereco.numero} Cidade: ${alunos[2].endereco.cidade} teve a Média: ${alunos[2].media}`);
 console.log(`\nLista dos Alunos(as) que foram Aprovados ou Reprovados:`);
-console.log(`\nO Aluno: ${alunos[0].nome} ${alunos[0].sobrenome} foi APROVADO com a Média: ${alunos[0].media}`);
-console.log(`\nA Aluna: ${alunos[2].nome} ${alunos[2].sobrenome} foi APROVADA com a Média: ${alunos[2].media}`);
-console.log(`\nO Aluno: ${alunos[1].nome} ${alunos[1].sobrenome} foi REPROVADO com a Média: ${alunos[1].media}`);
+console.log(`\nO Aluno: ${alunos[0].nome} ${alunos[0].sobrenome} da Sala: ${alunos[0].sala} foi APROVADO com a Média: ${alunos[0].media}`);
+console.log(`\nA Aluna: ${alunos[2].nome} ${alunos[2].sobrenome} da Sala: ${alunos[2].sala} foi APROVADA com a Média: ${alunos[2].media}`);
+console.log(`\nO Aluno: ${alunos[1].nome} ${alunos[1].sobrenome} da Sala: ${alunos[1].sala} foi REPROVADO com a Média: ${alunos[1].media}`);
---
+++ JS/Aula_07_Estruturas_Condicionais/Exercício_Aula_07/estruturasCondicionais.js
@@ -0,0 +1,27 @@
+const colaboradores = [
+    {
+        nome: 'Bruno',
+        salario: 1354,
+    },
+
+    {
+        nome: 'Francisco',
+        salario: 2530,
+    },
+
+    {
+        nome: 'Camila',
+        salario: 2675,
+    },
+
+    {
+        nome: 'Amanda',
+        salario: 3860,
+    },
+
+    {
+        nome: 'Jeferson',
+        salario: 8540,
+    },
+];
+
