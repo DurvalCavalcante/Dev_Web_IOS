d7abb26c528265996b1bac42dd8edba5db1026ed
DurvalCavalcante <durvalcavalcante00@gmail.com>
2022-05-17 15:52:37 -0300

    Exercício Aula 09 de JS

--- JS/Aula_09_Métodos/Exercício_em_Sala/emSala.js
+++ JS/Aula_09_Métodos/Exercício_em_Sala/emSala.js
@@ -6,35 +6,45 @@
 
 const alunos = [
     {
-        nome: 'Durval',
+        nome: 'Durval Cavalcante',
         idade: 23,
         turno: 'Noite',
         nota: 8,
     },
 
     {
-        nome: 'João',
+        nome: 'João Francisco',
         idade: 19,
         turno: 'Noite',
         nota: 5,
     },
 
     {
-        nome: 'Maria',
+        nome: 'Maria da Silva',
         idade: 22,
         turno: 'Manhã',
         nota: 9,
     },
 
     {
-        nome: 'Priscila',
+        nome: 'Priscilla Alcantara',
         idade: 20,
         turno: 'Tarde',
         nota: 6,
     }
 ];
 
-console.log(alunos.filter((name) => {
-    return name.nome;
+console.log('Nome de todos os Alunos(as):');
+alunos.forEach((nomeAlunos) => console.log(`\nAlunos(as): ${nomeAlunos.nome}`));
+
+const notasAlunos = alunos.filter((notas) => {
+    return notas.nota >= 6;
 })
+
+console.log(notasAlunos);
+
+console.log(
+    alunos.find((menor) => {
+        return menor.nota == 5;
+    })
 )
