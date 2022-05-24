fc0c3cea2c39a3572c38e8475c74c1c883db6b79
DurvalCavalcante <durvalcavalcante00@gmail.com>
2022-05-24 14:18:52 -0300

    Alterando o Exercício Aula 12 de JS

--- JS/Aula_12_JavaScript_HTML_DOM_Prt_2/Exercício_Aula_12/exercicioAula_12.js
+++ JS/Aula_12_JavaScript_HTML_DOM_Prt_2/Exercício_Aula_12/exercicioAula_12.js
@@ -14,42 +14,55 @@ for (let b = 0; b < botao.length; b++) {
 }
 
 id_1.addEventListener('click', clicar_01);
-id_2.addEventListener('click', clicar_02);
-id_3.addEventListener('click', clicar_03);
-
 // Utilizando o DOM, insira uma imagem da série Umbrella Academy ao lado do botão e mude a cor da body.
+
 function clicar_01() {
     id_1.innerText = ' The Umbrella Academy';
     document.body.style.background = '#262525';
-    let container = document.querySelector('#container');
-    let div_01 = document.createElement('div');
-    div_01.classList = 'div1';
-    let img_01 = document.querySelector('#container #B_02');
-    container.insertBefore(div_01, img_01);
-    div_01.innerHTML = "<img src='IMG/TheUmbrellaAcademy_logo.png'>";
+
+    let sectionImg = document.getElementById('imagem');
+    let imagem = document.createElement('img');
+
+    let nome = sectionImg.getElementsByTagName('img'); // remove a imagem duplicada ao clicar várias vezes.
+    if (nome.length) return;
+
+    imagem.src = './IMG/TheUmbrellaAcademy_logo.png';
+    sectionImg.appendChild(imagem);
 }
 
+id_2.addEventListener('click', clicar_02);
 /* terá que aparecer um prompt no browser solicitando o nome do usuário, e quando o nome for informado e o usuário clicar em OK,
 o nome deverá aparecer abaixo do botão com uma mensagem. */
 
 function clicar_02() {
-    let nome = prompt('Informe aqui seu Nome e Sobrenome:');
-    let container = document.querySelector('#container');
-    let texto = document.createElement('p');
-    texto.classList = 'texto';
-    let texto_01 = document.querySelector('#container #B_03');
-    container.insertBefore(texto, texto_01);
-    texto.innerHTML = `Olá ${nome}! Bem-vindo a nossa academia.`;
-    texto.style.color = 'white';
+    let sectionNome = document.getElementById('nomePrompt');
+    let promptName = prompt('Informe aqui seu Nome e Sobrenome:');
+    let paragrafo = document.createElement('p');
+
+    let nome = sectionNome.getElementsByTagName('p');
+
+    if (nome.length) {
+        nome[0].remove();         // remove o nome se o usuário adicionar mais do que uma vez.
+    }
+
+    paragrafo.innerHTML = `Olá ${promptName}, Bem-vindo a nossa Academia!`;
+    sectionNome.appendChild(paragrafo);
+    paragrafo.style.color = 'white';
 }
 
+id_3.addEventListener('click', clicar_03);
 /*No browser deve aparecer um prompt perguntando um número para o usuário logo após informar o número ele deve 
 retornar abaixo do botão a tabuada do número informado.*/
 
 function clicar_03() {
     let contador = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-    let receber = prompt('Digite aqui um número de 1 a 10:')
+    let receber = parseInt(prompt('Digite aqui um número de 1 a 10:'));  // o parsenInt, converte uma string para número.
+
+    if (isNaN(receber)) {
+        alert("Valor inválido");  // se o texto não for um número, ele não aceita.
+        return;
+    }
 
     do {
         console.log(`\n${receber} X ${contador[0]} = ${receber * contador[0]}`);
