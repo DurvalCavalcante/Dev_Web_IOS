2544bb95f4e922fbbd71d5f860afd3df9542f86a
DurvalCavalcante <durvalcavalcante00@gmail.com>
2022-05-28 21:31:49 -0300

    Alterações

--- JS/Aula_15_Manipulando_DOM_Prt_5/Exercício_Aula_15/JS/bootStrapDOM.js
+++ JS/Aula_15_Manipulando_DOM_Prt_5/Exercício_Aula_15/JS/bootStrapDOM.js
@@ -1,57 +1,22 @@
 const formAdd = document.getElementById('addForm');
 const itemList1 = document.getElementById('itens1');
 const itemList2 = document.getElementById('itens2');
-const filter = document.getElementById('filter');
 const input1 = document.getElementById('item1');
 const input2 = document.getElementById('item2');
 const input3 = document.getElementById('item3');
 const listaCarrinho = document.getElementById('listaCarrinho');
-
-
-const itens = [
-    {
-        item: 'Arroz',
-        quantidade: 1,
-        valor: '6,00',
-    },
-
-    {
-        item: 'Feijão',
-        quantidade: 1,
-        valor: '7,00'
-    },
-
-    {
-        item: 'Macarrão',
-        quantidade: 1,
-        valor: '4,00'
-    },
-
-    {
-        item: 'Leite',
-        quantidade: 1,
-        valor: '8,00'
-    },
-
-    {
-        item: 'Açucar',
-        quantidade: 1,
-        valor: '5,00',
-    }
-];
 
 formAdd.addEventListener('submit', addItem);
 
 // Adicionando Itens
 function addItem(e) {
     e.preventDefault();
-
     let itensAdd = document.getElementById('item1').value;
     let quantidadeAdd = document.getElementById('item2').value;
     let valorAdd = document.getElementById('item3').value;
     let li = document.createElement('li');
     li.classList = 'list-group-item';
-    li.appendChild(document.createTextNode(`Item: ${itensAdd} \\ Quantidade: ${quantidadeAdd} \\ R$ ${valorAdd},00`));
+    li.appendChild(document.createTextNode(`Item: ${itensAdd} \\ \\Quantidade: ${quantidadeAdd}  \\ \\ R$ ${valorAdd},00`));
 
     let deletarBtn = document.createElement('button');
     deletarBtn.className = 'btn btn-danger m-2 btn-sm float-end deletar';
@@ -83,3 +48,5 @@ function removeItem(e) {
         itemList1.removeChild(li);
     }
 }
+
+// Adicionando Item
