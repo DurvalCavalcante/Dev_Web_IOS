7d7682e06513a2390b2fab231dc4bfe8d74b4fda
DurvalCavalcante <durvalcavalcante00@gmail.com>
2022-06-02 19:06:58 -0300

    Alterações

--- React/Aula_03_Exercício/exercicio-aula-03/src/App.js
+++ React/Aula_03_Exercício/exercicio-aula-03/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import BoasVindas from './Components/BoasVindas';
 import Sobre from './Components/Sobre';
+document.body.style.background = '#0d192b';
 
 function App() {
   return (
