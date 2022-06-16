ce4323eca4606bd6d3cdf751785348cbaa62b716
DurvalCavalcante <durvalcavalcante00@gmail.com>
2022-06-16 15:46:30 -0300

    Exercício Aula 02 de Java

---
+++ Java/Aula_02_Tipos_De_Operadores/Aula_02_Exercício/Projeto_01_Calculando_a_Media/src/CalculandoMedia.java
@@ -0,0 +1,11 @@
+public class CalculandoMedia {
+    // método main inicia a execução do aplicativo
+    public static void main(String[] args) {
+        System.out.println("Resultado do calculo da media das notas");
+        System.out.println("Primeira Nota: " + 8);
+        System.out.println("Segunda Nota: " + 9);
+        System.out.println("Terceira Nota: " + 7);
+        System.out.println("Quarta Nota: " + 5);
+        System.out.println("Resultado do calculo: " + (8 + 9 + 7 + 5) / 4);
+    } // fim do método main
+}  // fim da classe
---
+++ Java/Aula_02_Tipos_De_Operadores/Aula_02_Exercício/Projeto_02_Calculando_a_Area/src/CalculandoArea.java
@@ -0,0 +1,8 @@
+public class CalculandoArea {
+    public static void main(String[] args) {
+        System.out.println("Calculando a Area de um triangulo retangulo");
+        System.out.println("Base: " + 25);
+        System.out.println("Altura: " + 35);
+        System.out.println("Resultado do calculo: " + (25 + 35) / 2);
+    }
+}
