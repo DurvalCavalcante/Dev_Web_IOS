25a9b51c93284fb9188a52f7e4a75be18990e237
DurvalCavalcante <durvalcavalcante00@gmail.com>
2022-06-28 22:58:51 -0300

    Alteração Exercício 07 de Java

--- Java/Aula_07_Teste_De_Mesa/Projeto_Teste_de_Mesa/src/br/com/aluno/Banco.java
+++ Java/Aula_07_Teste_De_Mesa/Projeto_Teste_de_Mesa/src/br/com/aluno/Banco.java
@@ -4,48 +4,61 @@ import java.util.Scanner;
 
 public class Banco {
     public static void main(String[] args) {
-        int opcao, escolha;
-
-        System.out.println("==== DBank ====");
-        System.out.println("\n==== Escolha o tipo de conta ========");
-        System.out.println(" 1 - Conta Corrente ");
-        System.out.println(" 2 - Conta Poupança ");
-        System.out.println(" 3 - Conta Estudante ");
-        System.out.println("======================================");
-
         Scanner teclado = new Scanner(System.in);
-        System.out.println("\n- Digite aqui o número da sua escolha: ");
-        opcao = teclado.nextInt();
+        char escolha = 'n';
 
-        switch (opcao) {
-            case 1:
-                System.out.println("\n======================================");
-                System.out.println("- Você selecionou a Conta Corrente ");
-                System.out.println("- Bem-vindo ao DBank               ");
+        while (escolha == 'n' || escolha == 'N') {
+            System.out.println("\n=============== DBank ===============");
+            System.out.println("\n==== Escolha o tipo de conta ========");
+            System.out.println(" 1 - Conta Corrente ");
+            System.out.println(" 2 - Conta Poupança ");
+            System.out.println(" 3 - Conta Estudante ");
+            System.out.println("======================================");
+
+            System.out.println("\n- Digite aqui o número da sua escolha: ");
+            int opcao = teclado.nextInt();
+            if (opcao <= 3) {
+                switch (opcao) {
+                    case 1:
+                        System.out.println("\n======================================");
+                        System.out.println("- Você selecionou a Conta Corrente ");
+                        System.out.println("- Bem-vindo ao DBank               ");
+                        System.out.println("======================================");
+                        break;
+                    case 2:
+                        System.out.println("\n======================================");
+                        System.out.println("- Você selecionou a Conta Poupança ");
+                        System.out.println("- Bem-vindo ao DBank               ");
+                        System.out.println("======================================");
+                        break;
+                    case 3:
+                        System.out.println("\n======================================");
+                        System.out.println("- Você selecionou a Conta Estudante ");
+                        System.out.println("- Bem-vindo ao DBank               ");
+                        System.out.println("======================================");
+                        break;
+                    default:
+                        System.out.println("- Escolha uma opção válida! ");
+                }
+
+
+                System.out.println("\n==== Deseja confirma sua opção ? =====");
+                System.out.println("- Digite: ");
+                System.out.println("S para Sim");
+                System.out.println("N para Não");
                 System.out.println("======================================");
-                break;
-            case 2:
-                System.out.println("\n======================================");
-                System.out.println("- Você selecionou a Conta Poupança ");
-                System.out.println("- Bem-vindo ao DBank               ");
-                System.out.println("======================================");
-                break;
-            case 3:
-                System.out.println("\n======================================");
-                System.out.println("- Você selecionou a Conta Estudante ");
-                System.out.println("- Bem-vindo ao DBank               ");
-                System.out.println("======================================");
-                break;
-            default:
-                System.out.println("- Escolha uma opção válida! ");
+                System.out.println("\n- Digite aqui o número da escolha:");
+                escolha = teclado.next().charAt(0);
+
+                if (escolha == 's' || escolha == 'S') {
+                    System.out.println("\n======================================");
+                    System.out.println("- Obrigado, É volte sempre !!");
+                    System.out.println("======================================");
+                }
+            } else {
+                System.out.println("\n- Escolha uma opção válida!");
+            }
         }
-
-        System.out.println("\n==== Deseja confirma sua opção ? =====");
-        System.out.println("1 - Sim");
-        System.out.println("2 - Não");
-        System.out.println("======================================");
-        System.out.println("\n- Digite aqui o número da escolha: ");
-        escolha = teclado.nextInt();
 
         teclado.close();
     }
